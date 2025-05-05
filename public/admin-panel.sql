1b846660cc7808f7d20db9db01646dbb6a194cdc
CytNestTeam <123726625+CytNestTeam@users.noreply.github.com>
2025-05-05 18:06:44 +0530

    Create admin-panel.sql

---
+++ public/admin-panel.sql
@@ -0,0 +1,475 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 05, 2025 at 02:36 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `admin-panel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menus`
+--
+
+CREATE TABLE `menus` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `created_by` int(11) NOT NULL,
+  `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp(),
+  `updated_by` int(11) DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `menus`
+--
+
+INSERT INTO `menus` (`id`, `name`, `type`, `project_id`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES
+(1, 'Dashboard', 'dashboard', 1, '2025-05-05 16:13:55', 1, NULL, NULL, NULL, NULL),
+(2, 'User Management', 'management', 1, '2025-05-05 16:13:55', 1, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pages`
+--
+
+CREATE TABLE `pages` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `route` varchar(255) NOT NULL,
+  `menu_id` int(11) NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `created_by` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pages`
+--
+
+INSERT INTO `pages` (`id`, `name`, `route`, `menu_id`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES
+(1, 'User List', '/users', 2, '2025-05-05 16:14:25', 1, NULL, NULL, NULL, NULL),
+(2, 'Roles', '/roles', 2, '2025-05-05 16:14:25', 1, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `page_apis`
+--
+
+CREATE TABLE `page_apis` (
+  `id` int(11) NOT NULL,
+  `action` enum('view','create','edit','delete') NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `page_id` int(11) NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `created_by` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `page_apis`
+--
+
+INSERT INTO `page_apis` (`id`, `action`, `path`, `page_id`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES
+(1, 'view', '/api/users', 1, '2025-05-05 16:15:21', 1, NULL, NULL, NULL, NULL),
+(2, 'create', '/api/users/create', 1, '2025-05-05 16:15:21', 1, NULL, NULL, NULL, NULL),
+(3, 'edit', '/api/users/edit', 1, '2025-05-05 16:15:21', 1, NULL, NULL, NULL, NULL),
+(4, 'delete', '/api/users/delete', 1, '2025-05-05 16:15:21', 1, NULL, NULL, NULL, NULL),
+(5, 'view', '/api/roles', 2, '2025-05-05 16:15:21', 1, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `page_forms`
+--
+
+CREATE TABLE `page_forms` (
+  `id` int(11) NOT NULL,
+  `page_id` int(11) NOT NULL,
+  `fields` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`fields`)),
+  `created_at` datetime DEFAULT current_timestamp(),
+  `created_by` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permission`
+--
+
+CREATE TABLE `permission` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `api_id` int(11) DEFAULT NULL,
+  `createdAt` datetime(3) NOT NULL DEFAULT current_timestamp(3),
+  `updatedAt` datetime(3) DEFAULT NULL ON UPDATE current_timestamp(3)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `permission`
+--
+
+INSERT INTO `permission` (`id`, `name`, `api_id`, `createdAt`, `updatedAt`) VALUES
+(1, 'view_dashboard', NULL, '2025-05-05 12:09:32.000', '2025-05-05 16:41:36.850'),
+(2, 'access_admin', NULL, '2025-05-05 12:38:09.000', '2025-05-05 16:41:40.329'),
+(4, 'user_view', 1, '2025-05-05 16:29:30.881', NULL),
+(5, 'user_create', 2, '2025-05-05 16:29:30.881', NULL),
+(6, 'user_edit', 3, '2025-05-05 16:29:30.881', NULL),
+(7, 'user_delete', 4, '2025-05-05 16:29:30.881', NULL),
+(8, 'role_view', 5, '2025-05-05 16:29:30.881', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projects`
+--
+
+CREATE TABLE `projects` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `created_by` int(11) NOT NULL,
+  `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp(),
+  `updated_by` int(11) DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `projects`
+--
+
+INSERT INTO `projects` (`id`, `name`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES
+(1, 'Admin Panel', '2025-05-05 16:13:32', 1, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL,
+  `name` varchar(191) NOT NULL,
+  `createdAt` datetime(3) NOT NULL DEFAULT current_timestamp(3),
+  `updatedAt` datetime(3) DEFAULT NULL ON UPDATE current_timestamp(3)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`id`, `name`, `createdAt`, `updatedAt`) VALUES
+(1, 'admin', '2025-05-01 14:22:47.000', NULL),
+(2, 'user', '2025-05-01 14:22:47.000', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rolepermission`
+--
+
+CREATE TABLE `rolepermission` (
+  `id` int(11) NOT NULL,
+  `roleId` int(11) NOT NULL,
+  `permissionId` int(11) NOT NULL,
+  `createdAt` datetime DEFAULT current_timestamp(),
+  `updatedAt` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `rolepermission`
+--
+
+INSERT INTO `rolepermission` (`id`, `roleId`, `permissionId`, `createdAt`, `updatedAt`) VALUES
+(1, 1, 1, '2025-05-05 12:10:37', NULL),
+(2, 1, 2, '2025-05-05 12:38:23', NULL),
+(3, 1, 4, '2025-05-05 16:32:48', NULL),
+(4, 1, 5, '2025-05-05 16:32:48', NULL),
+(5, 1, 6, '2025-05-05 16:32:48', NULL),
+(6, 1, 7, '2025-05-05 16:32:48', NULL),
+(7, 1, 8, '2025-05-05 16:32:48', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(191) NOT NULL,
+  `password` varchar(191) NOT NULL,
+  `roleId` int(11) NOT NULL DEFAULT 2,
+  `resetToken` varchar(255) DEFAULT NULL,
+  `resetTokenExpiry` datetime DEFAULT NULL,
+  `createdAt` datetime(3) NOT NULL DEFAULT current_timestamp(3),
+  `updatedAt` datetime(3) DEFAULT NULL ON UPDATE current_timestamp(3)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `password`, `roleId`, `resetToken`, `resetTokenExpiry`, `createdAt`, `updatedAt`) VALUES
+(1, 'arevee0001@gmail.com', '$2b$10$8m1URnzWaBfwuXYEGHVMfOrEi2fh5mvxCQgpceZjvUrWdk3STRsg6', 1, NULL, NULL, '2025-05-01 09:47:58.641', '2025-05-02 16:13:51.183'),
+(2, 'cytnest@gmail.com', '$2b$10$W4AEqw4/OcYorZbKsHV41.IXn5TSl4.KbKcgVvmkSTfiASRzvCVCO', 2, '56da193b-5e0d-4b96-bba8-f92b9e9fbd77', '2025-05-02 07:16:18', '2025-05-01 10:17:10.342', '2025-05-02 11:46:18.859');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userpermission`
+--
+
+CREATE TABLE `userpermission` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `permissionId` int(11) NOT NULL,
+  `createdAt` datetime DEFAULT current_timestamp(),
+  `updatedAt` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `userpermission`
+--
+
+INSERT INTO `userpermission` (`id`, `userId`, `permissionId`, `createdAt`, `updatedAt`) VALUES
+(1, 1, 1, '2025-05-05 12:20:16', NULL),
+(2, 2, 4, '2025-05-05 16:33:45', NULL),
+(3, 2, 8, '2025-05-05 16:33:45', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `menus`
+--
+ALTER TABLE `menus`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_project_id` (`project_id`);
+
+--
+-- Indexes for table `pages`
+--
+ALTER TABLE `pages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_menu_id` (`menu_id`);
+
+--
+-- Indexes for table `page_apis`
+--
+ALTER TABLE `page_apis`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_page_id` (`page_id`);
+
+--
+-- Indexes for table `page_forms`
+--
+ALTER TABLE `page_forms`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `page_id` (`page_id`);
+
+--
+-- Indexes for table `permission`
+--
+ALTER TABLE `permission`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`),
+  ADD KEY `idx_permission_name` (`name`),
+  ADD KEY `idx_permissions_api_id` (`api_id`);
+
+--
+-- Indexes for table `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rolepermission`
+--
+ALTER TABLE `rolepermission`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_role_permission` (`roleId`,`permissionId`),
+  ADD KEY `permissionId` (`permissionId`),
+  ADD KEY `idx_role_permission_roleId` (`roleId`),
+  ADD KEY `id` (`id`,`roleId`,`permissionId`,`createdAt`,`updatedAt`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `roleId` (`roleId`);
+
+--
+-- Indexes for table `userpermission`
+--
+ALTER TABLE `userpermission`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_user_permission` (`userId`,`permissionId`),
+  ADD KEY `permissionId` (`permissionId`),
+  ADD KEY `idx_user_permission_userId` (`userId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `menus`
+--
+ALTER TABLE `menus`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `pages`
+--
+ALTER TABLE `pages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `page_apis`
+--
+ALTER TABLE `page_apis`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `page_forms`
+--
+ALTER TABLE `page_forms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `permission`
+--
+ALTER TABLE `permission`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `projects`
+--
+ALTER TABLE `projects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `rolepermission`
+--
+ALTER TABLE `rolepermission`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `userpermission`
+--
+ALTER TABLE `userpermission`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `menus`
+--
+ALTER TABLE `menus`
+  ADD CONSTRAINT `menus_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`);
+
+--
+-- Constraints for table `pages`
+--
+ALTER TABLE `pages`
+  ADD CONSTRAINT `pages_ibfk_1` FOREIGN KEY (`menu_id`) REFERENCES `menus` (`id`);
+
+--
+-- Constraints for table `page_apis`
+--
+ALTER TABLE `page_apis`
+  ADD CONSTRAINT `page_apis_ibfk_1` FOREIGN KEY (`page_id`) REFERENCES `pages` (`id`);
+
+--
+-- Constraints for table `page_forms`
+--
+ALTER TABLE `page_forms`
+  ADD CONSTRAINT `page_forms_ibfk_1` FOREIGN KEY (`page_id`) REFERENCES `pages` (`id`);
+
+--
+-- Constraints for table `permission`
+--
+ALTER TABLE `permission`
+  ADD CONSTRAINT `apis_id` FOREIGN KEY (`api_id`) REFERENCES `page_apis` (`id`);
+
+--
+-- Constraints for table `rolepermission`
+--
+ALTER TABLE `rolepermission`
+  ADD CONSTRAINT `rolepermission_ibfk_1` FOREIGN KEY (`roleId`) REFERENCES `role` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `rolepermission_ibfk_2` FOREIGN KEY (`permissionId`) REFERENCES `permission` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`roleId`) REFERENCES `role` (`id`);
+
+--
+-- Constraints for table `userpermission`
+--
+ALTER TABLE `userpermission`
+  ADD CONSTRAINT `userpermission_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `userpermission_ibfk_2` FOREIGN KEY (`permissionId`) REFERENCES `permission` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
